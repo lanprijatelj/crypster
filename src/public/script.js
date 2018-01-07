945ffe7ac318ec358eaf275da3ef8abf3c92cc51
Lan Prijatelj <lan.prijatelj@gmail.com>
2018-01-07 17:07:55 +0100

    1

--- src/public/script.js
+++ src/public/script.js
@@ -1,9 +1,13 @@
 var price = {};
 var difficulty = {}; 
+var selectedCurrency = "";
+
 $(window).on("load", function(){
+    selectedCurrency = $("select[name=currency]").val();
     $.post("/diff").done(function (response) {
         difficulty.btc = response.difficulty;
         console.log(difficulty);
+        $("#diff").val(difficulty.btc);
     });
 
     $.post("/price").done(function (response) {
@@ -11,5 +15,26 @@ $(window).on("load", function(){
         price.eth = response.valueETH;
         price.ltc = response.valueLTC;
         console.log(price);
+        if(selectedCurrency == "BTC"){
+            $("#value").val(price.btc);
+        }else if(selectedCurrency == "ETH"){
+            $("#value").val(price.eth);
+        }else{
+            $("#value").val(price.ltc);
+        }
     });
+});
+
+$("select[name=currency]").change(function(){
+    selectedCurrency = $("select[name=currency]").val();
+    if(selectedCurrency == "BTC"){
+        $("#value").val(price.btc);
+        $("#hashRateUnit").val("TH/s");
+    }else if(selectedCurrency == "ETH"){
+        $("#value").val(price.eth);
+        $("#hashRateUnit").val("GH/s");
+    }else{
+        $("#value").val(price.ltc);
+        $("#hashRateUnit").val("MH/s");
+    }    
 });
