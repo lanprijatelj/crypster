5835a6c46e755c7a44397dc2ca4054fce3ca03b5
Lan Prijatelj <lan.prijatelj@gmail.com>
2018-01-19 22:24:40 +0100

    vsi diffi

--- src/public/script.js
+++ src/public/script.js
@@ -1,25 +1,36 @@
 var price = {};
 var difficulty = {};
-var userInput = {}; 
+var userInput = {};
 var results = {};
 
-function calculateMiningProfit(input){    
-    if(input.hashrateUnit == "TH/s"){
+function calculateMiningProfit(input) {
+    if (input.hashrateUnit == "TH/s") {
         var hashrate = input.hashrate * Math.pow(10, 12);
-    }else if(input.hashrateUnit == "GH/s"){
-        var hashrate = input.hashrate * Math.pow(10, 9);       
-    }else{
-        var hashrate = input.hashrate * Math.pow(10, 6);       
+    } else if (input.hashrateUnit == "GH/s") {
+        var hashrate = input.hashrate * Math.pow(10, 9);
+    } else {
+        var hashrate = input.hashrate * Math.pow(10, 6);
     }
-    return (hashrate * (1 - (input.fee / 100)) * (1 - (input.reject / 100)) * input.reward / (Math.pow(2,32) * input.diff)) * input.value;
+    return (hashrate * (1 - (input.fee / 100)) * (1 - (input.reject / 100)) * input.reward / (Math.pow(2, 32) * input.diff)) * input.value;
 }
 
-$(window).on("load", function(){
+$(window).on("load", function () {    
     userInput.selectedCurrency = $("select[name=currency]").val();
-    $.post("/diff").done(function (response) {
-        difficulty.btc = response.difficulty;
+
+    $.post("/diffBTC").done(function (response) {
+        difficulty.btc = response.difficultyBTC;
         //console.log(difficulty);
         $("#diff").val(difficulty.btc);
+    });
+
+    $.post("/diffETH").done(function (response) {
+        difficulty.eth = response.difficultyETH[0].difficulty;
+        //console.log(difficulty);        
+    });
+
+    $.post("/diffLTC").done(function (response) {
+        difficulty.ltc = response.difficultyLTC;
+        //console.log(difficulty);       
     });
 
     $.post("/price").done(function (response) {
@@ -27,31 +38,40 @@ $(window).on("load", function(){
         price.eth = response.valueETH;
         price.ltc = response.valueLTC;
         //console.log(price);
-        if(userInput.selectedCurrency == "BTC"){
+        if (userInput.selectedCurrency == "BTC") {
             $("#value").val(price.btc);
-        }else if(userInput.selectedCurrency == "ETH"){
+        } else if (userInput.selectedCurrency == "ETH") {
             $("#value").val(price.eth);
-        }else{
+        } else {
             $("#value").val(price.ltc);
         }
     });
 });
 
-$("select[name=currency]").change(function(){
+$("select[name=currency]").change(function () {
     userInput.selectedCurrency = $("select[name=currency]").val();
-    if(userInput.selectedCurrency == "BTC"){
+    if (userInput.selectedCurrency == "BTC") {
+        $("#valuta").text("BTC");
         $("#value").val(price.btc);
+        $("#diff").val(difficulty.btc);
         $("#hashRateUnit").val("TH/s");
-    }else if(userInput.selectedCurrency == "ETH"){
+    } else if (userInput.selectedCurrency == "ETH") {
+        $("#valuta").text("ETH");
         $("#value").val(price.eth);
+        $("#diff").val(difficulty.eth);
         $("#hashRateUnit").val("GH/s");
-    }else{
+    } else {
+        $("#valuta").text("LTC");
         $("#value").val(price.ltc);
+        $("#diff").val(difficulty.ltc);
         $("#hashRateUnit").val("MH/s");
-    }    
+    }
 });
 
-$("#calcMain").change(function(){
+$("#calcMain").change(sendParameters);
+$("input[name=hashrate]").keyup(sendParameters);
+
+function sendParameters() {
     userInput.hashrate = $("input[name=hashrate]").val();
     userInput.fee = $("input[name=fee]").val();
     userInput.reject = $("input[name=reject]").val();
@@ -71,25 +91,25 @@ $("#calcMain").change(function(){
     results.miningProfitM = results.miningProfitD * 30;
     results.miningProfitY = results.miningProfitM * 12;
 
-    $(".h :nth-child(2)").html((results.miningProfitH / userInput.value).toFixed(7));  
+    $(".h :nth-child(2)").html((results.miningProfitH / userInput.value).toFixed(7));
     $(".h :nth-child(3)").html((results.miningProfitH).toFixed(2));
-    $(".h :nth-child(4)").html((hourlyCost).toFixed(2));  
+    $(".h :nth-child(4)").html((hourlyCost).toFixed(2));
     $(".h :nth-child(5)").html((results.miningProfitH - hourlyCost).toFixed(2));
-    $(".d :nth-child(2)").html((results.miningProfitD / userInput.value).toFixed(2));  
+    $(".d :nth-child(2)").html((results.miningProfitD / userInput.value).toFixed(2));
     $(".d :nth-child(3)").html((results.miningProfitD).toFixed(2));
-    $(".d :nth-child(4)").html((hourlyCost * 24).toFixed(2));  
+    $(".d :nth-child(4)").html((hourlyCost * 24).toFixed(2));
     $(".d :nth-child(5)").html((results.miningProfitD - (hourlyCost * 24)).toFixed(2));
-    $(".w :nth-child(2)").html((results.miningProfitW / userInput.value).toFixed(2));  
+    $(".w :nth-child(2)").html((results.miningProfitW / userInput.value).toFixed(2));
     $(".w :nth-child(3)").html((results.miningProfitW).toFixed(2));
-    $(".w :nth-child(4)").html((hourlyCost * 168).toFixed(2));  
+    $(".w :nth-child(4)").html((hourlyCost * 168).toFixed(2));
     $(".w :nth-child(5)").html((results.miningProfitW - (hourlyCost * 168)).toFixed(2));
     $(".m :nth-child(2)").html((results.miningProfitM / userInput.value).toFixed(2));
     $(".m :nth-child(3)").html((results.miningProfitM).toFixed(2));
-    $(".m :nth-child(4)").html((hourlyCost * 720).toFixed(2));  
+    $(".m :nth-child(4)").html((hourlyCost * 720).toFixed(2));
     $(".m :nth-child(5)").html((results.miningProfitM - (hourlyCost * 720)).toFixed(2));
-    $(".y :nth-child(2)").html((results.miningProfitY / userInput.value).toFixed(2));  
+    $(".y :nth-child(2)").html((results.miningProfitY / userInput.value).toFixed(2));
     $(".y :nth-child(3)").html((results.miningProfitY).toFixed(2));
-    $(".y :nth-child(4)").html((hourlyCost * 8760).toFixed(2));  
+    $(".y :nth-child(4)").html((hourlyCost * 8760).toFixed(2));
     $(".y :nth-child(5)").html((results.miningProfitY - (hourlyCost * 8760)).toFixed(2));
-    console.log(results.miningProfitS);
-});
+    //console.log(results.miningProfitS);
+}
--- src/server.js
+++ src/server.js
@@ -23,29 +23,74 @@ app.listen(PORT, error => (
 ));
 
 
-app.post('/diff', function (req, res) {
-  var querry = "https://blockchain.info/charts/difficulty?timespan=1days&format=json";
+app.post('/diffBTC', function (req, res) {
+  var queryBTC = "https://blockchain.info/charts/difficulty?timespan=1days&format=json";  
   var podatki = "";
   var odg = {};
   request({
       method: 'GET',
-      uri: querry,
+      uri: queryBTC,
       encoding: null
     }, function (error, response, body) {
     }).on('data', function (data) {
       podatki += data;
     }).on('error', function (error) {
       console.log(error);
-      odg.difficulty = -1;
+      odg.difficultyBTC = -1;
       res.send(odg);
     }).on('end', function () {
       if (podatki[0] != "<") {
         var odgovor = JSON.parse(podatki);
-        odg.difficulty = odgovor.values[0].y;
-        res.send(odg);
-      } else {
-        odg.difficulty = 5000;
-        res.send(odg);
+        odg.difficultyBTC = odgovor.values[0].y;
+        res.send(odg);        
+      }
+    });
+});
+
+app.post('/diffLTC', function (req, res) {  
+  var queryLTC = "http://explorer.litecoin.net/chain/Litecoin/q/getdifficulty";
+  var podatki = "";
+  var odg = {};
+  request({
+      method: 'GET',
+      uri: queryLTC,
+      encoding: null
+    }, function (error, response, body) {
+    }).on('data', function (data) {
+      podatki += data;
+    }).on('error', function (error) {
+      console.log(error);
+      odg.difficultyLTC = -1;
+      res.send(odg);
+    }).on('end', function () {
+      if (podatki[0] != "<") {
+        var odgovor = JSON.parse(podatki);
+        odg.difficultyLTC = odgovor;
+        res.send(odg);        
+      }
+    });
+});
+
+app.post('/diffETH', function (req, res) {  
+  var queryETH = "https://www.etherchain.org/api/difficulty";
+  var podatki = "";
+  var odg = {};
+  request({
+      method: 'GET',
+      uri: queryETH,
+      encoding: null
+    }, function (error, response, body) {
+    }).on('data', function (data) {
+      podatki += data;
+    }).on('error', function (error) {
+      console.log(error);
+      odg.difficultyETH = -1;
+      res.send(odg);
+    }).on('end', function () {
+      if (podatki[0] != "<") {
+        var odgovor = JSON.parse(podatki);
+        odg.difficultyETH = odgovor;
+        res.send(odg);        
       }
     });
 });
