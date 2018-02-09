27b64466d82720828b4144581a5b135aa13a7b5d
Aljaz <aljaz.klanecek@gmail.com>
2018-02-09 22:54:08 +0100

    Added coin icon and minor design changes

--- src/public/script.js
+++ src/public/script.js
@@ -260,6 +260,7 @@ $jq(window).on("load", function () {
     });
 
       $jq("#my_popup").css({"margin-top": "0"});
+      $jq("#btcLogo").css({'display': 'block'});
 });
 
 /*resposive design-----------------------------------*/
@@ -292,19 +293,25 @@ $jq("select[name=currency]").change(function () {
     $jq("input[name=hashrate]").val(""); 
     $jq("#BE").text("0"); 
     $jq(".sols").css({"display" : "none"}); 
-    $jq(".hashes").css({"display" : "block"}); 
+    $jq(".hashes").css({"display" : "block"});
+    $jq("#btcLogo").css({'display': 'none'});
+    $jq("#ltcLogo").css({'display': 'none'});
+    $jq("#ethLogo").css({'display': 'none'});
+    $jq("#zecLogo").css({'display': 'none'});
     if (userInput.selectedCurrency == "BTC") {
         $jq("#valuta").text("BTC");
         $jq("#value").val(price.btc);
         $jq("input[name=reward]").val("12.5");
         $jq("#diff").val(difficulty.btc);
-        $jq("#hashRateUnit").val("TH/s");        
+        $jq("#hashRateUnit").val("TH/s");
+        $jq("#btcLogo").css({'display': 'block'});
     } else if (userInput.selectedCurrency == "ETH") {
         $jq("#valuta").text("ETH");
         $jq("input[name=reward]").val("3");
         $jq("#value").val(price.eth);
         $jq("#diff").val(difficulty.eth);
-        $jq("#hashRateUnit").val("MH/s");        
+        $jq("#hashRateUnit").val("MH/s");
+        $jq("#ethLogo").css({'display': 'block'});
     } else if (userInput.selectedCurrency == "ZEC") {
         $jq("#valuta").text("ZEC");
         $jq("input[name=reward]").val("10");
@@ -312,13 +319,15 @@ $jq("select[name=currency]").change(function () {
         $jq("#diff").val(difficulty.zec);
         $jq(".hashes").css({"display" : "none"});
         $jq(".sols").css({"display" : "block"});
-        $jq("#hashRateUnit").val("Sols/s");        
+        $jq("#hashRateUnit").val("Sols/s");
+        $jq("#zecLogo").css({'display': 'block'});
     } else {
         $jq("#valuta").text("LTC");
         $jq("#value").val(price.ltc);
         $jq("input[name=reward]").val("25");
         $jq("#diff").val(difficulty.ltc);
-        $jq("#hashRateUnit").val("MH/s");        
+        $jq("#hashRateUnit").val("MH/s");
+        $jq("#ltcLogo").css({'display': 'block'});
     }
 });
 
